ed7b17934d67687fb3bccb4dea77f432374a842b
Dheeren <110242985+DheerenGaud@users.noreply.github.com>
2023-02-12 00:04:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import  Home from "./Components/home"
 import  Login from "./Components/login"
-import  SinUp from "./Components/singup"
+import  SsinUp from "./Components/sSingUp"
+import  CsinUp from "./Components/cSingUp"
+import  Tutorials from "./Components/tutorials"
 
 function App() {
   return (
@@ -9,7 +11,9 @@ function App() {
                 <Routes>
                   <Route  path="/" element={<Home/>}/>
                   <Route  path="/login" element={<Login/>}/>
-                  <Route  path="/singup" element={< SinUp/>}/>
+                  <Route  path="/Ssingup" element={< SsinUp/>}/>
+                  <Route  path="/Csingup" element={< CsinUp/>}/>
+                  <Route  path="/tutorial/:id" element={< Tutorials/>}/>
                 </Routes>
               
            </Router>
