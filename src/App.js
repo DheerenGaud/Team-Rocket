927e8e31d5cedcb00dd82e771c341bb703dcf46c
Dheeren <110242985+DheerenGaud@users.noreply.github.com>
2023-02-12 11:42:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import  Home from "./Components/home"
 import  Login from "./Components/login"
 import  SsinUp from "./Components/sSingUp"
 import  CsinUp from "./Components/cSingUp"
-import  Tutorials from "./Components/tutorials"
+import './App.css'
+import  NavBar from "./Components/navbar"
+import  Vedios from "./Components/vedio"
 
 function App() {
   return (
@@ -13,7 +15,11 @@ function App() {
                   <Route  path="/login" element={<Login/>}/>
                   <Route  path="/Ssingup" element={< SsinUp/>}/>
                   <Route  path="/Csingup" element={< CsinUp/>}/>
-                  <Route  path="/tutorial/:id" element={< Tutorials/>}/>
+                  <Route  path="/tutorial/:id" element={< NavBar/>}>
+                      <Route  path="class/:id" element={< NavBar/>}/>
+                      <Route  path="class/:id" element={< NavBar/>}/>
+                  </Route>
+                  <Route  path="/tutorial/:id/vedios/:subId" element={<Vedios/>}/>
                 </Routes>
               
            </Router>
